0e7638edbf6664e0cfe84e9716fc4c8ec9e5e3cd
Tuan Le <vietle.tuan@gmail.com>
2025-02-20 10:41:56 -0800

    first commit

---
+++ scripts/myFunctions.js
@@ -0,0 +1,25 @@
+// JavaScript Document
+/* Student's First and Last Name
+	Current Date
+	My Function List
+*/
+// Thanks you alert
+function tyAlert(){
+	alert("Thank you for your interest in our fitness club. We will contact you soon.");
+}
+
+// Burpees video window
+function burpees(){
+	window.open("scripts/burpess.html", "_blank", "width=610, height=360");
+}
+
+// Plank video window
+function plank(){
+//	window.open("scripts/plank.html", "_blank", "width=610, height=360");
+	window.open("https://www.google.com", "_blank", "width=610, height=360");
+}
+
+//Mountain climber video window
+function mtnClimber(){
+	window.open("scripts/mtn.html", "_blank", "width=610, height=360");
+}
